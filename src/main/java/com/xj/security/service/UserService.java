c86f07e788b4c80e815d3349e73dc7e9aa3233ec
yu <1395759132@qq.com>
2020-03-23 22:36:41 +0800

    修复了bugger

--- src/main/java/com/xj/security/service/UserService.java
+++ src/main/java/com/xj/security/service/UserService.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public interface UserService extends UserDetailsService {
 
-    //public void save(SysUser user);
+    public void save(SysUser user);
 
     public List<SysUser> findAll();
 
--- src/main/java/com/xj/security/service/impl/UserServiceImpl.java
+++ src/main/java/com/xj/security/service/impl/UserServiceImpl.java
@@ -29,13 +29,13 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private RoleService roleService;
 
-   /* @Autowired
-    private BCryptPasswordEncoder passwordEncoder;*/
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-    /*public void save(SysUser user) {
+    public void save(SysUser user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userDao.save(user);
-    }*/
+    }
 
     public List<SysUser> findAll() {
         return userDao.findAll();
@@ -44,7 +44,7 @@ public class UserServiceImpl implements UserService {
     public Map<String, Object> toAddRolePage(Integer id) {
         List<SysRole> allRoles = roleService.findAll();
         List<Integer> myRoles = userDao.findRolesByUid(id);
-        Map<String, Object> map = new HashMap<String,Object>();
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("allRoles", allRoles);
         map.put("myRoles", myRoles);
         return map;
@@ -52,7 +52,7 @@ public class UserServiceImpl implements UserService {
 
     public void addRoleToUser(Integer userId, Integer[] ids) {
         userDao.removeRoles(userId);
-        if(ids.length>0){
+        if (ids.length > 0) {
             for (Integer rid : ids) {
                 userDao.addRoles(userId, rid);
             }
@@ -61,6 +61,7 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 认证业务
+     *
      * @param username 用户在浏览器输入的用户名
      * @return UserDetails 是springsecurity自己的用户对象
      * @throws UsernameNotFoundException
@@ -69,20 +70,19 @@ public class UserServiceImpl implements UserService {
         try {
             //根据用户名做查询
             SysUser sysUser = userDao.findByName(username);
-            if(sysUser==null){
+            if (sysUser == null) {
                 return null;
             }
             List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
-            //List<SysRole> roles = sysUser.getRoles();
-            //for (SysRole role : roles) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            List<SysRole> roles = sysUser.getRoles();
+            for (SysRole role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
 
-            //}
+            }
             //{noop}后面的密码，springsecurity会认为是原文。
-            UserDetails userDetails = new User(sysUser.getUsername(), "{noop}"+sysUser.getPassword(), authorities);
+            UserDetails userDetails = new User(sysUser.getUsername(), sysUser.getPassword(), authorities);
             return userDetails;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             //认证失败！
             return null;
